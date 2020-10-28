67bc596bbf7e737f319119340af131a135a6df4f
Abdul Moiz <moiza0799@gmail.com>
2020-10-28 20:00:41 +0500

    Initail Code

--- GNRSoftware/Form1.Designer.cs
+++ GNRSoftware/Form1.Designer.cs
@@ -29,12 +29,153 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lab_DateTime = new System.Windows.Forms.Label();
+            this.DateTimer = new System.Windows.Forms.Timer(this.components);
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.txt_BarCode = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lab_TotalGrnCount = new System.Windows.Forms.Label();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.lab_grnCount = new System.Windows.Forms.Label();
+            this.lab_ErrorMessage = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From Date: ";
+            // 
+            // lab_DateTime
+            // 
+            this.lab_DateTime.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lab_DateTime.AutoSize = true;
+            this.lab_DateTime.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_DateTime.Location = new System.Drawing.Point(16, 14);
+            this.lab_DateTime.Name = "lab_DateTime";
+            this.lab_DateTime.Size = new System.Drawing.Size(102, 36);
+            this.lab_DateTime.TabIndex = 1;
+            this.lab_DateTime.Text = "label2";
+            // 
+            // DateTimer
+            // 
+            this.DateTimer.Enabled = true;
+            this.DateTimer.Tick += new System.EventHandler(this.DateTimer_Tick);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(382, 19);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(130, 36);
+            this.btn_Reset.TabIndex = 2;
+            this.btn_Reset.Text = "RESET";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            // 
+            // txt_BarCode
+            // 
+            this.txt_BarCode.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_BarCode.Location = new System.Drawing.Point(21, 170);
+            this.txt_BarCode.Name = "txt_BarCode";
+            this.txt_BarCode.Size = new System.Drawing.Size(491, 33);
+            this.txt_BarCode.TabIndex = 3;
+            this.txt_BarCode.Text = "    112233   ";
+            this.txt_BarCode.TextChanged += new System.EventHandler(this.txt_BarCode_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lab_TotalGrnCount);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 18F);
+            this.groupBox1.Location = new System.Drawing.Point(22, 263);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(490, 194);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Total GRN";
+            // 
+            // lab_TotalGrnCount
+            // 
+            this.lab_TotalGrnCount.Font = new System.Drawing.Font("Century Gothic", 30F);
+            this.lab_TotalGrnCount.Location = new System.Drawing.Point(28, 92);
+            this.lab_TotalGrnCount.Name = "lab_TotalGrnCount";
+            this.lab_TotalGrnCount.Size = new System.Drawing.Size(439, 91);
+            this.lab_TotalGrnCount.TabIndex = 1;
+            this.lab_TotalGrnCount.Text = "0";
+            this.lab_TotalGrnCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Location = new System.Drawing.Point(382, 207);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(130, 36);
+            this.btn_Submit.TabIndex = 5;
+            this.btn_Submit.Text = "SUBMIT";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // lab_grnCount
+            // 
+            this.lab_grnCount.Font = new System.Drawing.Font("Century Gothic", 32F);
+            this.lab_grnCount.Location = new System.Drawing.Point(50, 72);
+            this.lab_grnCount.Name = "lab_grnCount";
+            this.lab_grnCount.Size = new System.Drawing.Size(439, 91);
+            this.lab_grnCount.TabIndex = 6;
+            this.lab_grnCount.Text = "0";
+            this.lab_grnCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lab_ErrorMessage
+            // 
+            this.lab_ErrorMessage.AutoSize = true;
+            this.lab_ErrorMessage.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_ErrorMessage.ForeColor = System.Drawing.Color.Red;
+            this.lab_ErrorMessage.Location = new System.Drawing.Point(17, 207);
+            this.lab_ErrorMessage.Name = "lab_ErrorMessage";
+            this.lab_ErrorMessage.Size = new System.Drawing.Size(84, 28);
+            this.lab_ErrorMessage.TabIndex = 7;
+            this.lab_ErrorMessage.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(550, 471);
+            this.Controls.Add(this.lab_ErrorMessage);
+            this.Controls.Add(this.lab_grnCount);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txt_BarCode);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.lab_DateTime);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GNR SOFTWARE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lab_DateTime;
+        private System.Windows.Forms.Timer DateTimer;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.TextBox txt_BarCode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lab_TotalGrnCount;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Label lab_grnCount;
+        private System.Windows.Forms.Label lab_ErrorMessage;
     }
 }
 
--- GNRSoftware/Form1.cs
+++ GNRSoftware/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,88 @@ namespace GNRSoftware
 {
     public partial class Form1 : Form
     {
+        int grnCount = 0, totalGrnCount = 0;
+        List<string> barcodeList = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void UpdateCount()
+        {
+            grnCount++;
+            totalGrnCount++;
+            lab_grnCount.Text = grnCount.ToString();
+            lab_TotalGrnCount.Text = totalGrnCount.ToString();
+        }
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+        }
+        private void InitializeSoftware()
+        {
+            try
+            {
+
+                using (var reader = new StreamReader(Application.StartupPath + @"\data.csv"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        string value = line[1].ToString();
+                        if (value == "0")
+                        {
+                            barcodeList.Add(line[0].ToString());
+                            totalGrnCount++;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private static void AddBarCodeToFile(string barcode)
+        {
+            using (StreamWriter sw = File.AppendText(Application.StartupPath + "\\data.csv"))
+            {
+                //FORMAT  barcode , bit , date
+                //if bit  = 0  :  reset == false else true
+                sw.WriteLine(barcode + ",0," + DateTime.Now.ToShortDateString());
+            }
+        }
+
+        private void txt_BarCode_TextChanged(object sender, EventArgs e)
+        {
+            string barcode = txt_BarCode.Text.Trim();
+            if (txt_BarCode.Text != "")
+            {
+
+                if (barcode.Length != 6) // bar code must be of 6 digits - client requirement
+                {
+                    lab_ErrorMessage.ForeColor = Color.Red;
+                    lab_ErrorMessage.Text = barcode + "-Invalid Barcode";
+                }
+                else
+                {
+                    lab_ErrorMessage.ForeColor = Color.LimeGreen;
+                    lab_ErrorMessage.Text = barcode + " Accepted";
+                    AddBarCodeToFile(barcode);
+                    UpdateCount();
+                }
+            }
+            txt_BarCode.Text = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeSoftware();
+        }
+
+        private void DateTimer_Tick(object sender, EventArgs e)
+        {
+            lab_DateTime.Text = DateTime.Now.ToString();
         }
     }
 }
